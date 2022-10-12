4d0f7201ee485d0f113a1a5ad5bb964e05f3ec0e
Apple-Kong <kongthn@gmail.com>
2022-10-12 15:25:11 +0900

    Initial Commit

---
+++ ClassManager/ClassManagerApp.swift
@@ -0,0 +1,17 @@
+//
+//  ClassManagerApp.swift
+//  ClassManager
+//
+//  Created by GOngTAE on 2022/10/12.
+//
+
+import SwiftUI
+
+@main
+struct ClassManagerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ ClassManager/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  ClassManager
+//
+//  Created by GOngTAE on 2022/10/12.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
