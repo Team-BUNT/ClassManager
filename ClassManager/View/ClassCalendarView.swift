383929cb73a852f3df2664ffd7093fa95c2cf18b
Cap0011 <59128435+Cap0011@users.noreply.github.com>
2022-10-13 00:26:08 +0900

    [FIX] Error while merging

--- ClassManager/View/ClassCalendarView.swift
+++ ClassManager/View/ClassCalendarView.swift
@@ -18,7 +18,6 @@ struct ClassCalendarView: View {
             from: DateComponents(year:2022, month: 10, day: 14, hour: 20, minute: 0)
         ),
         durationMinute: 60,
-        repetition: 1,
         hall: Hall(name: "Hall A", capacity: 40),
         applicantsCount: 30
     )
@@ -26,18 +25,6 @@ struct ClassCalendarView: View {
     var body: some View {
         NavigationView {
             VStack {
-                Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
-        //            .onAppear {
-        //                DataService.shared.createStudio(ID: "SampleID", name: "Something", location: "Somewhere", notice: DataService.DummyData.notice, halls: DataService.DummyData.halls)
-        //                DataService.shared.createClass(studioID: "SampleID", title: "POP-UP 클래스", instructorName: "Narae", date: Date(), durationMinute: 60, repetition: 4, hall: DataService.DummyData.hall)
-        //                Task {
-        //                    let studio = try await DataService.shared.requestStudioBy(studioID: "SampleID")
-        //                    print(studio)
-        //                    let classes = try await DataService.shared.requestAllClassesBy(studioID: "SampleID")
-        //                    print(classes)
-        //                }
-        //            }
-
                 DatePicker(
                     "Pick a class date",
                     selection: $date,
@@ -98,6 +85,16 @@ struct ClassCalendarView: View {
                 }
             }
         }
+        //            .onAppear {
+        //                DataService.shared.createStudio(ID: "SampleID", name: "Something", location: "Somewhere", notice: DataService.DummyData.notice, halls: DataService.DummyData.halls)
+        //                DataService.shared.createClass(studioID: "SampleID", title: "POP-UP 클래스", instructorName: "Narae", date: Date(), durationMinute: 60, repetition: 4, hall: DataService.DummyData.hall)
+        //                Task {
+        //                    let studio = try await DataService.shared.requestStudioBy(studioID: "SampleID")
+        //                    print(studio)
+        //                    let classes = try await DataService.shared.requestAllClassesBy(studioID: "SampleID")
+        //                    print(classes)
+        //                }
+        //            }
     }
 }
 
