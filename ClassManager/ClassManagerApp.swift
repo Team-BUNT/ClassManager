ac347ad9f0de0a4a7ea3fcb108c4f138c8efd858
Bethev <jmail88@naver.com>
2022-10-13 01:46:34 +0900

    [Fix] EnrollmentListView enrolledClass 수정

--- ClassManager/ClassManagerApp.swift
+++ ClassManager/ClassManagerApp.swift
@@ -29,7 +29,7 @@ struct YourApp: App {
                 OnboardingMain()
             } else {
                 NavigationView {
-                    ContentView()
+                    ClassCalendarView()
                 }
             }
         }
