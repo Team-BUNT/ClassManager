bc46d26c7fd47155f299c4973f48c613ef8de460
namgeon1106 <namgeon1106@naver.com>
2022-10-12 20:55:18 +0900

    [ADD] Enrollment.swift

---
+++ ClassManager/Model/Enrollment.swift
@@ -0,0 +1,16 @@
+//
+//  Enrollment.swift
+//  ClassManager
+//
+//  Created by 김남건 on 2022/10/12.
+//
+
+import Foundation
+
+struct Enrollment {
+    let ID: String
+    let classID: String?
+    let number: Int?
+    let enrolledDate: Date?
+    let paid: Bool?
+}
