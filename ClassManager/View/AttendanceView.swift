824c44d4f3306a202943e1cbc85fa9138cc1c8c8
Cap0011 <59128435+Cap0011@users.noreply.github.com>
2022-11-04 23:58:34 +0900

    [DESIGN] Add classList

--- ClassManager/View/AttendanceView.swift
+++ ClassManager/View/AttendanceView.swift
@@ -11,13 +11,24 @@ struct AttendanceView: View {
     let currentClass: Class
     
     @State var isPresentingConfirm = false
+    @State var isAllChecked = false
+    
+    let columnRatio: [Double] = [5/32, 7/32, 7/16, 3/16]
     
     var body: some View {
-        VStack {
-            classCard
+        ScrollView {
+            VStack(spacing: 0) {
+                classCard
+                studentList
+                    .padding(.top, 32)
+            }
+            .padding(20)
+            .foregroundColor(.white)
         }
-        .padding(20)
-        .foregroundColor(.white)
+        .navigationTitle("출석부")
+        .task {
+            // TODO: Fetch enrollments
+        }
     }
     
     var classCard: some View {
@@ -59,6 +70,109 @@ struct AttendanceView: View {
             .padding(20)
             .background(RoundedRectangle(cornerRadius: 10).foregroundColor(Color("Box")))
         }
+    }
+    
+    var studentList: some View {
+        VStack(alignment: .leading, spacing: 14) {
+            Text("수강생")
+                .font(.montserrat(.semibold, size: 17))
+            
+            HStack {
+                attendanceStatus
+                Spacer()
+                saveButton
+                    .onTapGesture {
+                        // TODO: Update attendance status
+                    }
+            }
+            .font(.montserrat(.semibold, size: 15))
+            
+            Divider()
+                .padding(.top, 6)
+            
+            studentListHeader
+            studentListBody
+        }
+    }
+    
+    var attendanceStatus: some View {
+        HStack(spacing: 8) {
+            HStack(spacing: 0) {
+                Text("출석 ")
+                    .font(.system(size: 15))
+                Text("11")
+            }
+            HStack(spacing: 0) {
+                Text("미출석 ")
+                    .font(.system(size: 15))
+                Text("5")
+            }
+            .foregroundColor(Color("Accent"))
+        }
+    }
+    
+    var saveButton: some View {
+        Text("저장")
+            .font(.system(size: 15))
+            .background(RoundedRectangle(cornerRadius: 7).frame(width: 60, height: 33).foregroundColor(Color("Box")))
+    }
+    
+    var studentListHeader: some View {
+        GeometryReader { geometry in
+            HStack(spacing: 10) {
+                Text("No.")
+                    .frame(width: (geometry.size.width - 30) * columnRatio[0])
+                Text("성명")
+                    .frame(width: (geometry.size.width - 30) * columnRatio[1])
+                Text("연락처")
+                    .frame(width: (geometry.size.width - 30) * columnRatio[2])
+                Text("출결상태")
+                    .frame(width: (geometry.size.width - 30) * columnRatio[3])
+            }
+            .foregroundColor(Color("DarkGray"))
+            .font(.system(size: 15))
+        }
+    }
+    
+    var studentListBody: some View {
+        GeometryReader { geometry in
+            VStack(spacing: 35) {
+                HStack(spacing: 10) {
+                    Text("All")
+                        .frame(width: (geometry.size.width - 30) * columnRatio[0])
+                        .font(.montserrat(.semibold, size: 15))
+                    Text("")
+                        .frame(width: (geometry.size.width - 30) * columnRatio[1])
+                    Text("")
+                        .frame(width: (geometry.size.width - 30) * columnRatio[2])
+                    
+                    ZStack {
+                        if isAllChecked {
+                            boxChecked
+                        } else {
+                            boxUnchecked
+                        }
+                    }
+                    .frame(width: (geometry.size.width - 30) * columnRatio[3])
+                    .onTapGesture {
+                        isAllChecked.toggle()
+                    }
+                }
+            }
+            .padding(.top, 20)
+        }
+    }
+    
+    var boxChecked: some View {
+        Image(systemName: "checkmark.square.fill")
+            .font(.system(size: 20))
+            .foregroundColor(Color("Accent"))
+    }
+    
+    var boxUnchecked: some View {
+        Image(systemName: "square")
+            .font(.system(size: 20))
+            .foregroundColor(Color("CheckGray"))
     }
 }
 
