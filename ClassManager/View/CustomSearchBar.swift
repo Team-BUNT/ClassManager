eb90741a6c5805d2b1238db9ae6342b8392acd37
namgeon1106 <namgeon1106@naver.com>
2022-11-05 22:31:22 +0900

    [FEAT]: implemented custom search bar

---
+++ ClassManager/View/CustomSearchBar.swift
@@ -0,0 +1,57 @@
+//
+//  CustomSearchBar.swift
+//  ClassManager
+//
+//  Created by 김남건 on 2022/11/05.
+//
+
+import SwiftUI
+
+struct CustomSearchBar: View {
+    @Binding var searchText: String
+    private let placeholderText = "수강생 이름을 검색하세요."
+    
+    var body: some View {
+        HStack(spacing: 6) {
+            Image(systemName: "magnifyingglass")
+                .foregroundColor(Color(.systemBackground))
+            ZStack {
+                if searchText.isEmpty {
+                    HStack {
+                        Text(placeholderText)
+                            .foregroundColor(Color("SearchText"))
+                            .font(.system(size: 15))
+                        Spacer()
+                    }
+                }
+                TextField("", text: $searchText)
+            }
+            Spacer(minLength: 0)
+            if !searchText.isEmpty {
+                Button {
+                    searchText = ""
+                } label: {
+                    Image(systemName: "xmark")
+                        .resizable()
+                        .foregroundColor(Color(UIColor.systemGray6))
+                        .frame(width: 8, height: 8)
+                        .background(Circle()
+                            .foregroundColor(Color(UIColor.systemGray2))
+                            .frame(width: 16, height: 16))
+                        .padding(.trailing, 4)
+                }
+                
+            }
+        }
+        .padding(.vertical, 7)
+        .padding(.horizontal, 8)
+        .background(RoundedRectangle(cornerRadius: 10).foregroundColor(Color("SearchBox")).opacity(0.24))
+    }
+}
+
+struct CustomSearchBar_Previews: PreviewProvider {
+    static var previews: some View {
+        CustomSearchBar(searchText: .constant(""))
+            .padding(.horizontal, 20)
+    }
+}
