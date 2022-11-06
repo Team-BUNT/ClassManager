db2d4c2c47886700b456f4eda9165018a635968e
namgeon1106 <namgeon1106@naver.com>
2022-11-06 16:29:36 +0900

    [FEAT]: implemented grouping logic of enrollments in PaymentStatusView

--- ClassManager/View/PaymentStatusView.swift
+++ ClassManager/View/PaymentStatusView.swift
@@ -10,6 +10,16 @@ import SwiftUI
 struct PaymentStatusView: View {
     @State private var searchText = ""
     private let paddingRatio: [CGFloat] = [41/107, 33/107, 33/107]
+    @State private var groupedEnrollments = [[Enrollment]]()
+    var filteredEnrollments: [[Enrollment]] {
+        if searchText.isEmpty {
+            return groupedEnrollments
+        }
+        
+        return groupedEnrollments.filter {
+            ( $0[0].userName ?? "" ).contains(searchText)
+        }
+    }
     
     var body: some View {
         NavigationView {
@@ -35,22 +45,25 @@ struct PaymentStatusView: View {
                         .padding(.horizontal, 10)
                         
                         ScrollView {
-                            LazyVStack(spacing: 38) {
-                                HStack(spacing: 0) {
-                                    Text("정윤성")
-                                        .frame(width: 44)
-                                        .padding(.trailing, (geometry.size.width - 247) * paddingRatio[0])
-                                    Text("010 2345 6789")
-                                        .font(.montserrat(.regular, size: 15))
-                                        .frame(width: 112)
-                                        .padding(.trailing, (geometry.size.width - 247) * paddingRatio[1])
-                                    Text("완료")
-                                        .frame(width: 60)
-                                        .padding(.trailing, (geometry.size.width - 247) * paddingRatio[2])
-                                    Image(systemName: "chevron.forward")
-                                        .font(.system(size: 12))
+                            LazyVStack(spacing: 0) {
+                                ForEach(filteredEnrollments, id: \.[0].ID) { enrollment in
+                                    HStack(spacing: 0) {
+                                        Text(enrollment[0].userName ?? "이름")
+                                            .frame(width: 44)
+                                            .padding(.trailing, (geometry.size.width - 247) * paddingRatio[0])
+                                        Text(enrollment[0].phoneNumber ?? "xxx xxxx xxxx")
+                                            .font(.montserrat(.regular, size: 15))
+                                            .frame(width: 112)
+                                            .padding(.trailing, (geometry.size.width - 247) * paddingRatio[1])
+                                        Text((enrollment[0].paid ?? false) ? "완료" : "대기")
+                                            .frame(width: 60)
+                                            .padding(.trailing, (geometry.size.width - 247) * paddingRatio[2])
+                                        Image(systemName: "chevron.forward")
+                                            .font(.system(size: 12))
+                                            .foregroundColor(Color("DarkGray"))
+                                    }
+                                    .padding(EdgeInsets(top: 19, leading: 10, bottom: 19, trailing: 10))
                                 }
-                                .padding(EdgeInsets(top: 19, leading: 10, bottom: 19, trailing: 10))
                             }
                         }
                     }
@@ -61,7 +74,74 @@ struct PaymentStatusView: View {
             .navigationBarTitleDisplayMode(.inline)
             .padding(.horizontal, 20)
             .font(.system(size: 15))
+            .task {
+                /*do {
+                    enrollments = try await DataService.shared.requestAllEnrollments(of: "BuntStudioSample") ?? []
+                } catch {
+                    print(error)
+                }*/
+                
+                groupedEnrollments = createGroupedEnrollments(from: dummyEnrollments)
+            }
         }
+    }
+    
+    private func createGroupedEnrollments(from enrollments: [Enrollment]) -> [[Enrollment]] {
+        let sortedEnrollments = enrollments.sorted(by: {
+            if $0.paid != $1.paid {
+                let paidToInt0 = ($0.paid ?? false) ? 1 : 0
+                let paidToInt1 = ($1.paid ?? false) ? 1 : 0
+                
+                return paidToInt0 < paidToInt1
+            }
+            
+            if $0.userName != $1.userName {
+                return $0.userName ?? "" < $1.userName ?? ""
+            }
+            
+            if $0.phoneNumber != $1.phoneNumber {
+                return $0.phoneNumber ?? "" < $1.phoneNumber ?? ""
+            }
+            
+            if $0.enrolledDate != $1.enrolledDate {
+                return $0.enrolledDate ?? Date() < $1.enrolledDate ?? Date()
+            }
+            
+            return $0.classID ?? "" < $1.classID ?? ""
+        })
+        
+        var groupedResult = [[Enrollment]]()
+        var temp = [Enrollment]()
+        for enrollment in sortedEnrollments {
+            if temp.isEmpty ||
+                (temp[0].phoneNumber == enrollment.phoneNumber
+                    && temp[0].paid == enrollment.paid) {
+                temp.append(enrollment)
+            } else {
+                groupedResult.append(temp)
+                temp = [enrollment]
+            }
+        }
+        
+        if !temp.isEmpty {
+            groupedResult.append(temp)
+        }
+        
+        return groupedResult
+    }
+}
+
+extension PaymentStatusView {
+    var dummyEnrollments: [Enrollment] {
+        [
+            Enrollment(ID: "Bunt-Class1-Enroll1", classID: "Bunt-Class1", userName: "김철수", phoneNumber: "01012340101", enrolledDate: Date(), paid: false, paymentType: "무통장"),
+            Enrollment(ID: "Bunt-Class1-Enroll2", classID: "Bunt-Class1", userName: "김영희", phoneNumber: "01012340102", enrolledDate: Date(), paid: true, paymentType: "무통장"),
+            Enrollment(ID: "Bunt-Class1-Enroll3", classID: "Bunt-Class1", userName: "김지수", phoneNumber: "01012340101", enrolledDate: Date(), paid: true, paymentType: "무통장"),
+            Enrollment(ID: "Bunt-Class2-Enroll1", classID: "Bunt-Class2", userName: "손흥민", phoneNumber: "01012340201", enrolledDate: Date(), paid: true, paymentType: "현장 카드"),
+            Enrollment(ID: "Bunt-Class3-Enroll1", classID: "Bunt-Class3", userName: "레이븐", phoneNumber: "01012340301", enrolledDate: Date(), paid: true, paymentType: "현장 카드"),
+            Enrollment(ID: "Bunt-Class4-Enroll1", classID: "Bunt-Class4", userName: "김철수", phoneNumber: "01012340101", enrolledDate: Date(), paid: false, paymentType: "현장 카드"),
+            Enrollment(ID: "Bunt-Class5-Enroll1", classID: "Bunt-Class5", userName: "김철수", phoneNumber: "01012340101", enrolledDate: Date(), paid: false, paymentType: "현장 카드"),
+        ]
     }
 }
 
