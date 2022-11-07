bafefaf864ea34a11870de9d07bbd9d8da1d449b
namgeon1106 <namgeon1106@naver.com>
2022-11-07 16:37:46 +0900

    [REFACTOR]: modified data model

--- ClassManager/Model/Student.swift
+++ ClassManager/Model/Student.swift
@@ -9,13 +9,16 @@ import Foundation
 
 struct Student {
     let ID: String
+    let studioID: String?
+    let phoneNumber: String?
     let subPhoneNumber: String? = nil
     let name: String?
-    let enrollments: [String]
+    let enrollments: [Enrollment]
     let coupons: [Coupon]
     
     struct Coupon {
         let studioID: String?
+        let studentID: String?
         let expiredDate: Date?
     }
 }
