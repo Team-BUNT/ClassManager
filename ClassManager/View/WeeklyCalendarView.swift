ecbda1c12301f27074ddcdecba9bff79bcbed357
Cap0011 <59128435+Cap0011@users.noreply.github.com>
2022-11-17 15:44:26 +0900

    [FEAT] filtering classes

--- ClassManager/View/WeeklyCalendarView.swift
+++ ClassManager/View/WeeklyCalendarView.swift
@@ -13,8 +13,10 @@ struct WeeklyCalendarView: View {
     
     let weekdays = ["월", "화", "수", "목", "금", "토", "일"]
     
+    let classes = Constant.shared.classes
+    
     var body: some View {
-        VStack(spacing: 20) {
+        VStack(spacing: 40) {
             HStack(alignment: .center, spacing: 12) {
                 Text("\(month)월")
                     .font(.montserrat(.semibold, size: 28))
@@ -33,13 +35,30 @@ struct WeeklyCalendarView: View {
                     }
             }
             .font(.system(size: 20))
-            ForEach(0..<7, id: \.self) { idx in
-                HStack {
-                    VStack {
-                        Text(weekdays[idx])
-                        Text(dayNumberString(weekday: idx))
+            HStack {
+                VStack {
+                    ForEach(0..<7, id: \.self) { idx in
+                        VStack {
+                            Text(weekdays[idx])
+                            Text(dayNumberString(weekday: idx))
+                        }
+                        Spacer()
                     }
-                    Spacer()
+                }
+                .padding(.vertical)
+                ScrollView(.horizontal) {
+                    VStack(alignment: .leading, spacing: 40) {
+                        ForEach(0..<7, id: \.self) { idx in
+                            HStack {
+                                ForEach(classesOnDay(day: dayOfWeek(weekday: idx)), id: \.self.ID) { danceClass in
+                                    VStack {
+                                        Text(danceClass.title ?? "")
+                                        Text(danceClass.instructorName ?? "")
+                                    }
+                                }
+                            }
+                        }
+                    }
                 }
             }
         }
@@ -48,16 +67,23 @@ struct WeeklyCalendarView: View {
         }
     }
     
-    private func dayNumberString(weekday: Int) -> String {
+    private func dayOfWeek(weekday: Int) -> Date {
         let currentWeekday = date.get(.weekday) == 1 ? 6 : date.get(.weekday) - 2
-        let calculatedDate = Calendar.current.date(byAdding: .day, value: weekday - currentWeekday, to: date)
-        return String(calculatedDate!.get(.day))
+        return Calendar.current.date(byAdding: .day, value: weekday - currentWeekday, to: date) ?? Date()
+    }
+    
+    private func dayNumberString(weekday: Int) -> String {
+        return String(dayOfWeek(weekday: weekday).get(.day))
     }
     
     private func monthNumber() -> Int {
         let currentWeekday = date.get(.weekday) == 1 ? 6 : date.get(.weekday) - 2
         let monday = Calendar.current.date(byAdding: .day, value: -currentWeekday, to: date)
         return monday?.get(.month) ?? 13
+    }
+    
+    private func classesOnDay(day: Date) -> [Class] {
+        return classes?.filter({ Calendar.current.isDate($0.date ?? Date(), inSameDayAs: day) }) ?? [Class]()
     }
 }
 
